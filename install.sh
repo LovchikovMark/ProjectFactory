f1e1c7fda3d40abfd8ed8b33f804b8f20db812e2
LovchikovMark <lovchikovmmark@gmail.com>
2025-12-09 22:45:59 +0300

    fix ./install.sh

--- install.sh
+++ install.sh
@@ -2,17 +2,17 @@
 
 echo "Installing ProjectFactory..."
 
-git clone https://raw.githubusercontent.com/LovchikovMark/ProjectFactory/master/bin/main /usr/local/bin/
+curl -O https://raw.githubusercontent.com/LovchikovMark/ProjectFactory/master/bin/main && mv main /usr/local/bin/
+
+sudo chmod +x /usr/local/bin/main
 
 if [[ -f ~/.bashrc ]]; then
-    echo "alias pf=' bash /usr/local/bin/main'" >> ~/.bashrc
+    echo "alias pf='/usr/local/bin/main'" >> ~/.bashrc
 fi
 
 if [[ -f ~/.zshrc ]]; then
-    echo "alias pf=' bash /usr/local/bin/main'" >> ~/.zshrc
+    echo "alias pf='/usr/local/bin/main'" >> ~/.zshrc
 fi
-
-source ~/.bashrc
 
 echo "✅ Installation complete!"
 echo "Usage: pf [command]"
