554926ef3b8775537cffe3c32cd03b6a41134bcd
Neo <lovchikovmmark@gmail.com>
2026-04-01 09:23:58 +0300

    add actions

--- main.py
+++ main.py
@@ -7,28 +7,73 @@ from pathlib import Path
 app = typer.Typer()
 
 @app.command(help="Create file stucture")
-def cfs(sample_file_path : str = typer.Option(False, '-f', help="Sample file path"), directory : str = typer.Argument(default=os.getcwd, help="target directory")):
+def cfs(sample_file_path : str = typer.Option(None, '-f', help="Sample file path"), my_template_name : str = typer.Option(None, '-t', help="Sample file path"),directory : str = typer.Argument(default=os.getcwd, help="target directory")):
+    
+    print(sample_file_path)
+    print(my_template_name)
+
+    if sample_file_path:
+
+        current_dir = Path.cwd()
+        json_path = (current_dir / sample_file_path).resolve()
+
+        try:
+
+            with open(json_path, 'r') as f:
+
+                data = yaml.safe_load(f)
+
+                Factory(data, path=str((current_dir / directory).resolve()))
+
+
+        except FileNotFoundError:
+
+            print('file not found')
+
+    elif my_template_name:
+
+        current_dir = Path.cwd()
+        json_path = (Path.home() / ".config" / 'pf' / (my_template_name + ".json")).resolve()
+
+        try:
+
+            with open(json_path, 'r') as f:
+
+                data = yaml.safe_load(f)
+
+                Factory(data, path=str((current_dir / directory).resolve()))
+
+
+        except FileNotFoundError:
+
+            print('file not found')
+
+@app.command(help="Save your template for future using")
+def smt(sample_file_path : str = typer.Option(False, '-f', help="Sample file path")):
 
     current_dir = Path.cwd()
-    yaml_path = (current_dir / sample_file_path).resolve()
+    json_path = (current_dir / sample_file_path).resolve()
 
     try:
 
-        with open(yaml_path, 'r') as f:
-
-            data = yaml.safe_load(f)
-
-            Factory(data, path=str((current_dir / directory).resolve()))
-
+        Path.copy_into(json_path, f"{Path.home()}/.config/pf")
 
     except FileNotFoundError:
 
         print('file not found')
-    
 
-@app.command()
-def goodbye(name : str = typer.Argument(..., help="Enter your name: ")):
+@app.command(help="Save your template for future using")
+def gmt():
 
-    typer.echo(name)
+    try:
+
+        for file in os.listdir(f"{Path.home()}/.config/pf"):
+
+            print(file.split('.json')[0])
+
+    except FileNotFoundError:
+
+        print('file not found')
+
 
 app()
