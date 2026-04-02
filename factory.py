4d5b8301a5f81d91b7a97464c2afbc8ea6285aac
Neo <lovchikovmmark@gmail.com>
2026-04-02 17:06:24 +0300

    fix release action and 'files' problem

--- factory.py
+++ factory.py
@@ -6,38 +6,49 @@ def Factory(data, path = os.getcwd()):
 
     try:
 
-        for folder_name in list(filter(lambda i: i != "files", list(data.keys()))):
+        folders = list(filter(lambda i: type(data[i]) == dict, list(data.keys())))
 
-            os.mkdir(path + "/" + folder_name)
-                    
-            if 'files' in data[folder_name] and data[folder_name]['files']:
+        if len(folders) != 0:
 
-                if isinstance(data[folder_name]['files'], list):
+            for folder_name in folders:
 
-                    for file_name in data[folder_name]['files']:
+                os.mkdir(path + "/" + folder_name)
+                        
+                if 'files' in data[folder_name] and data[folder_name]['files']:
 
-                        Path(path + "/" + folder_name + "/" + file_name).touch()
+                    if isinstance(data[folder_name]['files'], list):
 
-                elif isinstance(data[folder_name]['files'], dict):
+                        for file_name in data[folder_name]['files']:
 
-                    for file_name in list(data[folder_name]['files'].keys()):
+                            Path(path + "/" + folder_name + "/" + file_name).touch()
 
-                        Path(path + "/" + folder_name + "/" + file_name).touch()
+                    elif isinstance(data[folder_name]['files'], dict):
 
-                        with open(path + "/" + folder_name + "/" + file_name, 'w', encoding='utf-8') as f:
+                        for file_name in list(data[folder_name]['files'].keys()):
 
-                            f.write(data[folder_name]['files'][file_name])
+                            Path(path + "/" + folder_name + "/" + file_name).touch()
+
+                            with open(path + "/" + folder_name + "/" + file_name, 'w', encoding='utf-8') as f:
+
+                                f.write(data[folder_name]['files'][file_name])
 
 
 
-            if type(data[folder_name]) == str:
+                if type(data[folder_name]) == str:
 
-                shutil.copytree(path + '/' + data[folder_name], path + "/" + folder_name, dirs_exist_ok=True)
-            
-            else:
+                    shutil.copytree(path + '/' + data[folder_name], path + "/" + folder_name, dirs_exist_ok=True)
+                
+                else:
 
-                Factory(data[folder_name], path + "/" + folder_name)
-    
+                    Factory(data[folder_name], path + "/" + folder_name)
+        else:
+
+            if 'files' in data and data['files']:
+
+                for file_name in data['files']:
+
+                    Path(path + "/" + file_name).touch()
+
     except Exception as e:
 
         print(f'Error! {e}')
