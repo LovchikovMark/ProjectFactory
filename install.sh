e019cd4295eaffaddcd6cd8256c863d0a13af4a5
Neo <lovchikovmmark@gmail.com>
2026-04-02 16:08:22 +0300

    add actions

--- install.sh
+++ install.sh
@@ -2,17 +2,19 @@
 
 echo "Installing ProjectFactory..."
 
-curl -O https://raw.githubusercontent.com/LovchikovMark/ProjectFactory/master/bin/pf && mv pf /usr/local/bin/
+curl -O https://raw.githubusercontent.com/LovchikovMark/ProjectFactory/master/bin/pf && sudo mv pf /usr/local/bin/
 mkdir ~/.config/pf
 
-sudo chmod +x /usr/local/pf
+sudo chmod +x /usr/local/bin/pf
 
 if [[ -f ~/.bashrc ]]; then
     echo "alias pf='/usr/local/bin/pf'" >> ~/.bashrc
+    source ~/.bashrc
 fi
 
 if [[ -f ~/.zshrc ]]; then
     echo "alias pf='/usr/local/bin/pf'" >> ~/.zshrc
+    source ~/.zshrc
 fi
 
 echo "✅ Installation complete!"
